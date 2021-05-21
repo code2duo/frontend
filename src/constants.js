9a8df0787684144b8a780d0896feb06226f726c5
Debdut Goswami <debdutgoswami@gmail.com>
2021-05-22 01:59:45 +0530

    changed to firebase hosting

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const __prod__ = process.env.NODE_ENV === "prod"
 export const __api_base_url__ = "https://api.code2duo.co/api/v1"
 export const firebaseConfig = {
     apiKey: "AIzaSyA2NdV6_9wwUba779WjdBUGEAlAN1DI4Lw",
-    authDomain: "code2duo.firebaseapp.com",
+    authDomain: "app.code2duo.co",
     projectId: "code2duo",
     storageBucket: "code2duo.appspot.com",
     messagingSenderId: "958137059797",
